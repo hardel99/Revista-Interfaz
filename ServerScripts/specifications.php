dbf2687ccda17072def3c4d477da9f504747ef15
Harold Delgado <34784651+hardel99@users.noreply.github.com>
2019-08-17 16:24:47 -0600

    Create specifications.php

---
+++ ServerScripts/specifications.php
@@ -0,0 +1,6 @@
+<?php
+	define('DB_USER', "id1951096_hardel"); // db yuser
+	define('DB_PASSWORD', "interfaz6ff"); // db password
+	define('DB_DATABASE', "id1951096_revis"); // database namaeba
+	define('DB_SERVER', "localhost"); // db server
+?>
